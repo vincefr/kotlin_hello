8d9e1a9726b835f039494cd2c76326dca00ccf84
Vinc <vincent.barbotin@cewe.fr>
2017-06-26 12:37:14 +0200

    1ere version fichier hello.kt

---
+++ app/src/main/java/com/capturemoi/hellokotlin/Hello.kt
@@ -0,0 +1,12 @@
+package com.capturemoi.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Hello : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+    }
+}
